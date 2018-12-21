8f489e5be5d066d337c37fcc01c1e148521fb81f
Tinoco López <juan.d.tinoco.lopez@accenture.com>
2018-12-21 18:23:07 +0100

    v4

--- microservices/configservice/src/main/java/deors/demos/microservices/configservice/ConfigserviceApplication.java
+++ microservices/configservice/src/main/java/deors/demos/microservices/configservice/ConfigserviceApplication.java
@@ -1,16 +1,15 @@
+@org.springframework.cloud.config.server.EnableConfigServer
+
 package deors.demos.microservices.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@org.springframework.cloud.config.server.EnableConfigServer
 public class ConfigserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigserviceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserviceApplication.class, args);
+    }
 }
-
-
-@org.springframework.cloud.config.server.EnableConfigServer

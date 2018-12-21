fec3ff9f903fbdc6af496c29b06f7009fcf11dd6
Tinoco López <juan.d.tinoco.lopez@accenture.com>
2018-12-21 18:29:35 +0100

    v6

--- microservices/eurekaservice/src/main/java/deors/demos/microservices/eurekaservice/EurekaserviceApplication.java
+++ microservices/eurekaservice/src/main/java/deors/demos/microservices/eurekaservice/EurekaserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 public class EurekaserviceApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +12,3 @@ public class EurekaserviceApplication {
 	}
 
 }
-@org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
